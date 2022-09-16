be54c29c3780c0d1c3b7e0d0f2e444b70382c9d8
Your Name <you@example.com>
2022-09-15 19:11:38 -0700

    my tasks

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _isalpha - fuction to chheck if c isletter, lowercase or uppercase
+ * @c: is the int that will use for the argument for the function
+ * Return: 0 
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= '2') || (c >= 'A' && c <= '2'))
+	{
+	return (1);
+	}
+	else
+	return (0);
+}
+
