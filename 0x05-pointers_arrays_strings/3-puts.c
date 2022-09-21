b70bc2ed39eff64512e93776accd47d0e670ba21
Your Name <you@example.com>
2022-09-20 17:29:31 -0700

    Task2

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,15 +1,27 @@
-#include <stdio.h>
+#include <stdio.h.
 #include "main.h"
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
+ *_puts - prints a string
+ * @str: string to print
+ *
+ *Description: prints a string
+ *On success: returns no error
  */
- 
+
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++)
+	int i = 0;
 
-	_putchar('\n');
-}
+	while (*(str + i) != '\0')
+	{
+		putchar(*(str + i));
+		i++;
+	}
+	int n;
+
+	int n;
+
+	int n;
+
+        putchar(10);
