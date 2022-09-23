1bea6ee09ada1cc07d8695612cde709b4ad9b190
Your Name <you@example.com>
2022-09-22 20:37:01 -0700

    task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,38 +1,20 @@
-                                                                                                                                                                               
+#include "holberton.h"
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char *sh = "Holberton";
 
-/**                                                                                                                                                                            
+	while (*sh)
+	{
+		_putchar(*sh);
+		sh++;
+	}
+	_putchar('\n');
 
- * main print - putchar                                                                                                                                                        
+	return (0);
+}
 
- * Return: always 0                                                                                                                                                            
-
- */                                                                                                                                                                            
-
-                                                                                                                                                                               
-
-int main(void)                                                                                                                                                                 
-
-{                                                                                                                                                                              
-
-        _putchar('_');                                                                                                                                                         
-
-        _putchar('p');                                                                                                                                                         
-
-        _putchar('u');                                                                                                                                                         
-
-        _putchar('t');                                                                                                                                                         
-
-        _putchar('c');                                                                                                                                                         
-
-        _putchar('h');                                                                                                                                                         
-
-        _putchar('a');                                                                                                                                                         
-
-        _putchar('r');                                                                                                                                                         
-
-        _putchar('\n');                                                                                                                                                        
-
-                                                                                                                                                                               
-
-clear
-c
