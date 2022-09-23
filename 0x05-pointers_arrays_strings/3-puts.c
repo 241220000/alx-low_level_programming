ca9f463cb50d4bc5037d184f22fb660e92207b56
Your Name <you@example.com>
2022-09-23 04:01:24 -0700

    task 3

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,16 @@
-#include "main.h"
+#include "holberton.h"
 
 /**
- * _puts - Prints a string to stdout.
- * @str: The string to be printed.
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
  */
-void _puts(char *str)
+int main(void)
 {
-	while (*str)
-		_putchar(*str++);
+	char *str;
 
-	_putchar('\n');
+	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	_puts(str);
+	return (0);
 }
+
