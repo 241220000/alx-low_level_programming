9933735f7dbd1b2f2cb3d041ab41f226200e4bb4
Your Name <you@example.com>
2022-09-23 06:11:48 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,18 +1,18 @@
 #include "main.h"
-
 /**
  * _isupper - checks for uppercase characters.
  * @c: char
  * Return: either 0 or 1.
  */
-
 int _isupper(int c)
 {
-        if (c >= 'A' && c <= 'Z')
-        {
-               return (1);
-        }
-
-        return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
 
