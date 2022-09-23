a143d8a4aba75771c278c730400d09d7e5c97c32
Your Name <you@example.com>
2022-09-22 22:49:23 -0700

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,29 +1,13 @@
-Vi 4-puts.c
-
-Type in the following lines of code
-
-
-
 #include <stdio.h>
 
 /**
+ * main - Entry point
  *
- *  *main- Serves as the entry point for the program
- *
- *   *
- *
- *    *Return: By default returns zero if no erros otherwise returns non-zero value.
- *
- *     */
-
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
-
-	 puts("\"Programming is like building a multilingual puzzle");
-
-	  return (0);
-
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
-
 
