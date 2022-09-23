a504b6d5e38ebf200ba577c239c7cc2867cf4761
Your Name <you@example.com>
2022-09-23 03:14:09 -0700

    task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,16 +1,18 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * print_alphabet - fuction to print abc
- * Return: 0
+ * print_alphabet - a function that prints the alphabet, in lowercase
+ *
+ * Return: Always 0 (Succes)
  */
 void print_alphabet(void)
 {
-        char c;
+	int n;
 
-        for (c = 'a';c <= 'z'; c++)
-        {
-		_putchar(c);
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar(n);
 	}
-_putchar('\n');
+	_putchar('\n');
 }
 
