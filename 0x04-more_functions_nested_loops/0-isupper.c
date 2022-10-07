7cf67ee3fa9c263103dd7b25ef67a2d11e84c127
Your Name <you@example.com>
2022-10-07 02:10:31 -0700

    task 0

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,22 @@
 #include "main.h"
 
 /**
- * _isupper - define if a character is uppercase
- * Description: Prints the alphabet with _putchar
- * @c: charater
- * Return: void
+ * _isupper - checkes upper case
+ * @c: print int
+ * Return: give 1 or 0
  */
+
 int _isupper(int c)
 {
-	if ((c >= 'A') && (c <= 'Z'))
+	if (c >= 'A' && c <= 'Z')
+	{
 		return (1);
+	}
 
-	return (0);
+	else
+	{
+		return (0);
+	}
+
 }
 
